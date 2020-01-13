2952fc6969bd8413cdae09befec43e7c5e02826f
centos <centos@May.the.Bug.be.with.you>
2020-01-13 16:22:58 +0900

    .

--- install.sh
+++ install.sh
@@ -9,13 +9,13 @@ else
 fi
 
 if [ -e "$2" ]; then
-	echo -e "\e[0;11mUnpacking..."
+	echo -e "\e[40;93mUnpacking...\e[0m"
 	tar -axf $2 -C $instdir || exit 1
-	gtk-update-icon-cache /usr/share/icons/hicolor
+#	gtk-update-icon-cache /usr/share/icons/hicolor
 fi
 
 
-cat > /etc/profile.d/packettracer.sh <<-TRACER
+cat > /etc/profile.d/packettracer.sh <<- TRACER
 
 	export PT7HOME=$instdir/pt
 	export QT_DEVICE_PIXEL_RATIO=auto
@@ -23,7 +23,7 @@ cat > /etc/profile.d/packettracer.sh <<-TRACER
 TRACER
 
 
-cat > /usr/share/applications/cisco-pt7.desktop <<-CPT
+cat > /usr/share/applications/cisco-pt7.desktop <<- CPT
 	[Desktop Entry]
 	Type=Application
 	Exec=$instdir/pt/packettracer %f:
@@ -33,7 +33,7 @@ cat > /usr/share/applications/cisco-pt7.desktop <<-CPT
 	MimeType=application/x-pkt;application/x-pka;application/x-pkz;
 CPT
 
-cat > /usr/share/applications/cisco-ptsa7.desktop <<-CPT-SA
+cat > /usr/share/applications/cisco-ptsa7.desktop <<- CPT-SA
 	[Desktop Entry]
 	Type=Application
 	Exec=$instdir/pt/packettracer -uri=%u
@@ -46,19 +46,32 @@ CPT-SA
 update-desktop-database /usr/share/applications
 ln -sf $instdir/pt/packettracer /usr/local/bin/packettracer
 
-mkdir -p /usr/lib64/packettracer
+#mkdir -p /usr/lib64/packettracer
 
-cp $(dirname $BASH_SOURCE)/lib/* /usr/lib64/packettracer/
-ln -srf /usr/lib64/packettracer/libdouble-conversion.so.1.0 /usr/lib64/packettracer/libdouble-conversion.so.1
-ln -srf /usr/lib64/packettracer/libjpeg.so.8.2.2 /usr/lib64/packettracer/libjpeg.so.8
+#cp $(dirname $BASH_SOURCE)/lib/* /usr/lib64/packettracer/
+#ln -srf /usr/lib64/packettracer/libdouble-conversion.so.1.0 /usr/lib64/packettracer/libdouble-conversion.so.1
+#ln -srf /usr/lib64/packettracer/libjpeg.so.8.2.2 /usr/lib64/packettracer/libjpeg.so.8
 
-cat > /etc/ld.so.conf.d/cisco-pt7-x86_64.conf <<-LINKER
-	/usr/lib64/packettracer
-LINKER
-ldconfig
+#cat > /etc/ld.so.conf.d/cisco-pt7-x86_64.conf <<- LINKER
+#	/usr/lib64/packettracer
+#LINKER
 
+#ldconfig
+
+__OS_VER=$(awk -F= '/^VERSION_ID/ { print $2 }' /etc/os-release | tr -d \" | cut -d. -f1)
+__CWD=$(dirname $BASH_SOURCE)
+cp $__CWD/lib/libjpeg.so.8.2.2 $instdir/pt/bin/
+ln -srf $instdir/pt/bin/libjpeg.so.8.2.2 $instdir/pt/bin/libjpeg.so.8
+cp $__CWD/lib/libdouble-conversion.so.1.0 $instdir/pt/bin/
+ln -srf $instdir/pt/bin/libdouble-conversion.so.1.0 $instdir/pt/bin/libdouble-conversion.so.1
+
+if [ $__OS_VER -eq 7 ]; then
+	cp $__CWD/lib/libpng16.so.16.34.0 $instdir/pt/bin/
+	ln -srf $instdir/pt/bin/libpng16.so.16.34.0 $instdir/pt/bin/libpng16.so.16
+fi
 
 #/opt/pt/
+#/usr/local/bin/packettracer
 #/usr/share/applications/cisco-pt7.desktop
 #/usr/share/applications/cisco-ptsa7.desktop
 #/usr/share/icons/gnome/48x48/mimetypes/pka.png
